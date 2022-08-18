cfdb77682545f2f49e8a9686722a678c35e331c5
elijahmarco <elijahmakur@gmail.com>
2022-08-18 23:31:01 +0300

    adding li to ul function

--- code.js
+++ code.js
@@ -1,1 +1,19 @@
 let input = document.getElementById("inputs")
+let button = document.getElementById('enter')
+let ul = documetn.getElementById('ul')
+
+function inputLength(){
+    return input.length; 
+}
+function listLength(){
+    return item.length;
+}
+function createli(){
+    var li = document.createElement('li')
+    li.appendChild(document.createtextnode(input.value))
+    ul.appendChild(li)
+    input.value
+    
+
+}
+ //creating a function for creating a new element.
